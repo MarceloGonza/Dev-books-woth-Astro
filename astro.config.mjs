173abd3797db8788b8f6ef142688013ab6ceb668
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-04-10 11:23:54 -0300

    proyecto finalizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
-// @ts-check
 import { defineConfig, envField } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [tailwind()],
   env: {
     schema: {
-      SHOW_BUY_BUTTON: envField.boolean({
-        default: true,
+      SCORE_API_ENDPOINT: envField.string({
         context: "server",
         access: "public",
       }),
-      SCORE_API_ENDPOINT: envField.string({
+      SHOW_BUY_BUTTON: envField.boolean({
+        default: true,
         context: "server",
         access: "public",
       }),
